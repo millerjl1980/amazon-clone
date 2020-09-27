a33d892165c831f28acf909253bc521a3f2943dd
Justin Miller <indymiller@gmail.com>
2020-09-27 18:43:18 -0400

    Firebase config file added for project

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDk7xod4kA5e-XuKqxWV5dvpwGuiOd08tI",
+    authDomain: "clone-cdfbe.firebaseapp.com",
+    databaseURL: "https://clone-cdfbe.firebaseio.com",
+    projectId: "clone-cdfbe",
+    storageBucket: "clone-cdfbe.appspot.com",
+    messagingSenderId: "833374661490",
+    appId: "1:833374661490:web:1f25302d549593dcf74beb",
+    measurementId: "G-YMDS50SREM"
+  };
